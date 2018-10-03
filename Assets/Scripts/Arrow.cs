c0f363cd45129b83a157c36b1ad3556a7c11e805
L1nde <kodaniklinde@gmail.com>
2018-10-04 01:58:45 +0300

    Added archer and made it shoot first structure block

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Arrow : MonoBehaviour
+    {
+        public float initialAngle = 45f;
+
+        private Rigidbody2D rb2d;
+        private GameObject fortbase;
+
+        private bool flying = true;
+
+        // Use this for initialization
+        void Start()
+        {
+            fortbase = GameObject.FindGameObjectWithTag("StructureBlock");
+            rb2d = GetComponent<Rigidbody2D>();
+
+            Vector3 target = fortbase.transform.position;
+
+            float gravity = Physics.gravity.magnitude;
+            // Selected angle in radians
+            float angle = initialAngle * Mathf.Deg2Rad;
+
+
+            // Planar distance between objects
+            float distance = Vector3.Distance(target, transform.position);
+            // Distance along the y axis between objects
+            float yOffset = transform.position.y - target.y;
+
+            float initialVelocity = (1 / Mathf.Cos(angle)) *
+                                    Mathf.Sqrt((0.5f * gravity * Mathf.Pow(distance, 2)) /
+                                               (distance * Mathf.Tan(angle) + yOffset));
+
+            Vector3 velocity = new Vector3(0, initialVelocity * Mathf.Sin(angle), initialVelocity * Mathf.Cos(angle));
+
+            // Rotate our velocity to match the direction between the two objects
+            float angleBetweenObjects = Vector3.Angle(Vector3.forward, target - transform.position);
+            Vector3 finalVelocity = Quaternion.AngleAxis(angleBetweenObjects, Vector3.up) * velocity;
+            if (target.x - transform.position.x < 0)
+            {
+                finalVelocity = new Vector3(-finalVelocity.x, finalVelocity.y);
+            }
+
+            rb2d.AddForce(finalVelocity * rb2d.mass, ForceMode2D.Impulse);
+
+
+            transform.rotation = LookAt2D(rb2d.velocity);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!flying)
+            {
+                rb2d.velocity = Vector2.zero;
+            }
+            else
+            {
+                transform.rotation = LookAt2D(rb2d.velocity);
+            }
+
+            Destroy(gameObject, 10f);
+        }
+
+        private Quaternion LookAt2D(Vector2 forward)
+        {
+            return Quaternion.Euler(0, 0, Mathf.Atan2(forward.y, forward.x) * Mathf.Rad2Deg);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Terrain")
+            {
+                flying = false;
+            }
+
+//            gameObject.AddComponent<FixedJoint2D>().connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyArcher.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class EnemyArcher : MonoBehaviour
+    {
+        public float hp = 100f;
+        public float speed = 1f;
+
+        private Rigidbody2D rb2d;
+
+        private bool facingRight = true;
+
+        public float attackDelay = 1f;
+        private float attackAcc = 0f;
+        public Arrow arrowPreFab;
+
+        // Use this for initialization
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+            rb2d.velocity = new Vector2(speed * Time.deltaTime, rb2d.velocity.y);
+
+            // attacking
+            if (attackAcc > attackDelay)
+            {
+                attackAcc = 0f;
+                Instantiate(arrowPreFab, transform.position, transform.rotation);
+            }
+
+            attackAcc += Time.deltaTime;
+        }
+
+
+        // Flips the sprite
+        void Flip()
+        {
+            facingRight = !facingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+}
