ff2cfd42fac0114318d4e9cf115b1fa40e8ec6d2
Meelis Perli <meelis.perli@gmail.com>
2018-10-03 15:44:24 +0300

    Added hp to structure blocks

---
+++ Assets/Scripts/StructureBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureBlock : MonoBehaviour {
+
+    public float hp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hp <= 0)
+            DestroySelf();
+	}
+
+    private void DestroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+}
