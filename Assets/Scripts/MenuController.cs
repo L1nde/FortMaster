95c6ed0e8b1a52d607a652378b3e3daf234995f9
L1nde <kodaniklinde@gmail.com>
2018-10-28 00:30:52 +0300

    Initial main menu completed. Need to do loading waves. Hardcoded wave 0

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace Assets.Scripts {
+    public class MenuController : MonoBehaviour {
+
+        public Button play;
+
+        public Animator anim;
+
+        public void playButton() {
+            anim.Play("InitialMenu");
+        }
+
+        public void save0() {
+            anim.Play("TraitsMenu");
+        }
+
+        public void newGame() {
+            SceneManager.LoadScene("LindeScene");
+        }
+    }
+
+
+}
