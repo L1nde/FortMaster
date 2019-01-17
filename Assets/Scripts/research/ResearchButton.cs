b1f9a250bacd4f45205d3872e291d4cdf459fb56
MeelisP <meelis.perli@gmail.com>
2019-01-17 15:50:26 +0200

    Added tooltips

--- Assets/Scripts/research/ResearchButton.cs
+++ Assets/Scripts/research/ResearchButton.cs
@@ -15,6 +15,10 @@ public class ResearchButton : MonoBehaviour {
     public UnityEngine.Object researchable;
     public ResearchTreeNode nodeData;
 
+    public Image toolTipPrefab;
+
+    private Image currentToolTip;
+
     public void setResearchText(string text)
     {
         researchText.text = text;
@@ -68,6 +72,52 @@ public class ResearchButton : MonoBehaviour {
             GetComponent<Button>().interactable = !nodeData.researched && nodeData.prerequisites.ToList().TrueForAll(prerequisitesResearched);
         }
 
+    }
+
+    public void hoverEnter() {
+        if (currentToolTip != null)
+            Destroy(currentToolTip.gameObject);
+
+        currentToolTip = Instantiate(toolTipPrefab);
+        currentToolTip.transform.SetParent(FindObjectOfType<Canvas>().transform);
+        currentToolTip.transform.position = Input.mousePosition + new Vector3(60,-60,0);
+        //ja ja ja polümorfismiga on parem
+        if (researchable.GetType() == typeof(ResearchBlock)) {
+            createToolTipForResearchBlock((ResearchBlock)researchable);
+        } else if (researchable.GetType() == typeof(ResearchTurret)) {
+            createToolTipForTurret((ResearchTurret)researchable);
+        }
+    }
+
+    public void hoverExit() {
+        Destroy(currentToolTip.gameObject);
+        currentToolTip = null;
+    }
+
+    private void createToolTipForResearchBlock(ResearchBlock rb) {
+        addTextLineToParent("Type: structure block", currentToolTip.transform);
+        addTextLineToParent("Gold cost: " + rb.block.cost, currentToolTip.transform);
+        addTextLineToParent("HP: " + rb.block.hp, currentToolTip.transform);
+    }
+
+    private void createToolTipForTurret(ResearchTurret turret) {
+        addTextLineToParent("Type: structure block", currentToolTip.transform);
+        addTextLineToParent("Gold cost: " + turret.block.cost, currentToolTip.transform);
+        addTextLineToParent("Damage: " + turret.block.projectile.damage, currentToolTip.transform);
+        addTextLineToParent("Reload: " + turret.block.reloadTime, currentToolTip.transform);
+        addTextLineToParent("Range: " + turret.block.attackRange, currentToolTip.transform);
+    }
+
+    private void addTextLineToParent(string text, Transform parent) {
+        GameObject textObj = new GameObject("myTextGO");
+        textObj.transform.SetParent(parent);
+
+        Text myText = textObj.AddComponent<Text>();
+        Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        myText.font = ArialFont;
+        myText.rectTransform.sizeDelta = new Vector2(100, 18);
+        myText.color = new Color(0, 0, 0);
+        myText.text = text;
     }
 
     private bool prerequisitesResearched(ResearchTreeNode item) {
